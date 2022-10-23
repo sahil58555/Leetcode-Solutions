a22ec9ade00a934388ae3dd70570341a15a99e99
Sahil Saxena <96676636+sahil58555@users.noreply.github.com>
2022-10-23 19:05:20 +0530

    Time: 8 ms (47.13%), Space: 55.2 MB (21.57%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.java
+++ 645-set-mismatch/645-set-mismatch.java
@@ -1,59 +1,20 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         
-        int xor = 0;
-        int idx = 1;
+        int diff = 0;
+        int sqDiff = 0;
         
-        for(int num : nums) {
+        for(int i = 0 ; i < nums.length ; i++) {
             
-            xor = xor ^ num;
-            xor = xor ^ idx;
+            int curr = (i +  1);
             
-            idx++;
+            diff += curr - nums[i];
+            sqDiff += (curr * curr) - (nums[i] * nums[i]);
+            
         }
         
-        //System.out.println(xor);
+        int sum = sqDiff / diff;
         
-        int num1 = 0 , num2 = 0;
-        int mask = xor & (~xor + 1);
-        
-        idx = 1;
-        
-        for(int num : nums) {
-            
-            if((mask & num) == 0) {
-                
-                num1 = num1 ^ num;
-            }
-            else{
-                
-                num2 = num2 ^ num;
-            }
-            
-            if((mask & idx) == 0) {
-                
-                num1 = num1 ^ idx;
-            }
-            else{
-                
-                num2 = num2 ^ idx;
-            }
-            
-            idx++;
-        }
-        
-        for(int num : nums) {
-            
-            if(num == num1) {
-                
-                return new int[]{num1,num2};
-            }
-            else if(num == num2) {
-                
-                return new int[]{num2,num1};
-            }
-        }
-        
-        return null;
+        return new int[]{(sum - diff) / 2, (sum + diff) / 2};
     }
 }
