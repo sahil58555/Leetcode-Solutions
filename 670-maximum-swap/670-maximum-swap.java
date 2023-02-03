72ea18bdbc617c8c0b47f74058b7bb8ade3c2efd
Sahil Saxena <96676636+sahil58555@users.noreply.github.com>
2023-02-03 16:22:22 +0530

    Time: 1 ms (61.45%), Space: 39 MB (93.22%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maximumSwap(int num) {
+        
+        String maxNum = num + "";
+        char[] arr = maxNum.toCharArray();
+        int n = maxNum.length();
+        
+        int[] max = new int[n];
+        max[n - 1] = n - 1;
+        
+        for(int i = n - 2 ; i >= 0 ; i--) {
+            
+            if(arr[i] > arr[max[i + 1]]) {
+                
+                max[i] = i;
+            }
+            else {
+                
+                max[i] = max[i + 1];
+            }
+        }
+        
+        for(int i = 0 ; i < n ; i++) {
+            
+            if(arr[i] < arr[max[i]]) {
+                
+                swap(arr, i, max[i]);
+                String curr = new String(arr);
+                
+                if(curr.compareTo(maxNum) > 0) {
+                    
+                    maxNum = curr;
+                }
+                
+                swap(arr, i, max[i]);
+            }
+        }
+        
+        return Integer.parseInt(maxNum);
+    }
+    
+    private void swap(char[] arr, int idx1, int idx2) {
+        
+        char temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+    }
+}
