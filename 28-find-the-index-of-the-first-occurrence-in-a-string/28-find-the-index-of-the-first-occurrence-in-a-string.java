5b766084ad5073f683afa77bc937a2fef2054d26
Sahil Saxena <96676636+sahil58555@users.noreply.github.com>
2023-03-03 15:27:28 +0530

    Time: 4 ms (7.05%), Space: 40.8 MB (23.87%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,6 +1,53 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
+    public int strStr(String text, String pattern) {
         
-        return haystack.indexOf(needle);
+        int n = text.length();
+        int m = pattern.length();
+        
+        String str = pattern + "#" + text;
+        
+        int[] lps = getLPSArray(str);
+        
+        for(int i = m + 1 ; i < lps.length ; i++) {
+            
+            if(lps[i] == m) {
+                
+                return i - 2 * m;
+            }
+        }
+        
+        return -1;
+    }
+    
+    private int[] getLPSArray(String str) {
+        
+        int n = str.length();
+        int[] lps = new int[n];
+        
+        int i = 1 , length = 0;
+        
+        while(i < n) {
+            
+            if(str.charAt(i) == str.charAt(length)) {
+                
+                length++;
+                lps[i] = length;
+                i++;
+            }
+            else {
+                
+                if(length > 0) {
+                    
+                    length = lps[length - 1];
+                }
+                else {
+                    
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        
+        return lps;
     }
 }
