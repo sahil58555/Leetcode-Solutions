23177f94b6519a8b4d66ca762c2099b8c7397870
Sahil Saxena <96676636+sahil58555@users.noreply.github.com>
2023-03-30 22:21:44 +0530

    Time: 6 ms (87.86%), Space: 42.3 MB (89.74%) - LeetHub

--- 87-scramble-string/87-scramble-string.java
+++ 87-scramble-string/87-scramble-string.java
@@ -3,44 +3,46 @@ class Solution {
     private String s1;
     private String s2;
     private int n;
-    private Boolean[][][][] dp;
+    private Boolean[][][] dp;
     
     public boolean isScramble(String s1, String s2) {
         
         this.n = s1.length();
         this.s1 = s1;
         this.s2 = s2;
-        this.dp = new Boolean[n][n][n][n];
+        this.dp = new Boolean[n][n][n];
         
-        return isScramble(0, n - 1, 0, n - 1);
+        return isScramble(0, n - 1, 0);
     }
     
-    private boolean isScramble(int start1, int end1, int start2, int end2) {
+    private boolean isScramble(int start1, int end1, int start2) {
         
         if(start1 == end1) {
             
             return (s1.charAt(start1) == s2.charAt(start2));
         }
         
-        if(dp[start1][end1][start2][end2] != null) {
+        if(dp[start1][end1][start2] != null) {
             
-            return dp[start1][end1][start2][end2];
+            return dp[start1][end1][start2];
         }
+        
+        int end2 = start2 + end1 - start1;
         
         for(int i = start1 ; i < end1 ; i++) {
             
             int len = i - start1;
-            boolean option1 = isScramble(start1, i, start2, start2 + len) && isScramble(i + 1, end1, start2 + len + 1, end2);
+            boolean option1 = isScramble(start1, i, start2) && isScramble(i + 1, end1, start2 + len + 1);
             
             len = i - start1;
-            boolean option2 = isScramble(start1, i, end2 - len, end2) && isScramble(i + 1, end1, start2, end2 - len - 1);
+            boolean option2 = isScramble(start1, i, end2 - len) && isScramble(i + 1, end1, start2);
 
             if(option1 || option2) {
                 
-                return dp[start1][end1][start2][end2] = true;
+                return dp[start1][end1][start2] = true;
             }
         }
         
-        return dp[start1][end1][start2][end2] = false;
+        return dp[start1][end1][start2] = false;
     }
 }
