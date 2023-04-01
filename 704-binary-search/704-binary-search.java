d5c057be3d9552f238d78a5c398fa53d3e701fab
Sahil Saxena <96676636+sahil58555@users.noreply.github.com>
2023-04-01 10:20:43 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (27.40%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int search(int[] nums, int target) {
+     
+        int index = Arrays.binarySearch(nums, target);
+        
+        return index < 0 ? -1 : index;
+    }
+}
