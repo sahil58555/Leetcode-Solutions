f45abdab89b89e914b4ec321d38ba0b250bfadd9
Sahil Saxena <96676636+sahil58555@users.noreply.github.com>
2023-05-11 11:56:04 +0530

    Time: 20 ms (5.21%), Space: 42.6 MB (20.35%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -1,9 +1,24 @@
 class Solution {
+    
+    private Map<Integer, List<Integer>> index;
+    
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        
+        createIndexMap(nums2);
         
         Integer[][] dp = new Integer[nums1.length][nums2.length + 1];
         
         return sol(0, 0, nums1, nums2, dp);
+    }
+    
+    private void createIndexMap(int[] nums) {
+        
+        index = new HashMap<>();
+        
+        for(int i = 0 ; i < nums.length ; i++) {
+            
+            index.compute(nums[i], (key, val) -> val == null ? new ArrayList<>() : val).add(i);
+        }
     }
     
     private int sol(int idx, int maxIdx, int[] nums1, int[] nums2, Integer[][] dp) {
@@ -19,15 +34,20 @@ class Solution {
         }
         
         int notTake = sol(idx + 1, maxIdx, nums1, nums2, dp);
+        
+        List<Integer> listIndex = index.getOrDefault(nums1[idx], new ArrayList<>());
+        int eleIndex = Collections.binarySearch(listIndex, maxIdx);
+        
+        if(eleIndex < 0) {
+            
+            eleIndex = eleIndex * -1 - 1;
+        }
+
         int take = 0;
         
-        for(int i = maxIdx ; i < nums2.length ; i++) {
+        if(eleIndex < listIndex.size()) {
             
-            if(nums2[i] == nums1[idx]) {
-                
-                take = 1 + sol(idx + 1, i + 1, nums1, nums2, dp);
-                break;
-            }
+            take = 1 + sol(idx + 1, listIndex.get(eleIndex) + 1, nums1, nums2, dp);
         }
         
         return dp[idx][maxIdx] = Math.max(notTake, take);
